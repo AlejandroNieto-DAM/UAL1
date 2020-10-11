4a815d502dbe05e58a6c6b57f89145c027f683e2
NietOff <alejandrona5b@gmail.com>
2020-10-11 21:31:07 +0200

    Practica 1

---
+++ NietoAlarconAlejandroIP2021/src/org/ip/sesion01/DivisionPorCero.java
@@ -0,0 +1,14 @@
+package org.ip.sesion01;
+
+public class DivisionPorCero {
+
+	public static void main(String[] args) {
+		
+		System.out.println(17.0 + " / " + 0.0 + " = " + 17.0/0.0);
+		System.out.println(17.0 + " % " + 0.0 + " = " + 17.0%0.0);
+		System.out.println(17 + " / " + 0 + " = ");
+		System.out.println(17/0);
+		
+	}
+
+}
